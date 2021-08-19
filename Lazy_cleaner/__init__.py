8316421d2cb77d9e861bf2d9f5c5e51748cada02
Ahmed Ashraf Khlail <59618586+ahmed13131@users.noreply.github.com>
2021-08-19 19:27:04 +0200

    Add files via upload

---
+++ Lazy_cleaner/__init__.py
@@ -0,0 +1,5 @@
+from Lazy_cleaner.Lazy_cleaner import stat
+from Lazy_cleaner.Lazy_cleaner import fillnan
+from Lazy_cleaner.Lazy_cleaner import label
+from Lazy_cleaner.Lazy_cleaner import Clean_Data
+from Lazy_cleaner.Lazy_cleaner import fill_lin_rand
